e7ac5f91398c2f922d56978fde2e4c4ba93b51c4
GLAPPY <satwikv7@gmail.com>
2021-01-09 10:50:19 +0530

    added comments

--- zoom_bot.py
+++ zoom_bot.py
@@ -1,3 +1,4 @@
+# Importing useful modules
 from pathlib import Path
 import datetime
 import pyautogui
@@ -6,16 +7,21 @@ import pygetwindow as gw
 import keyboard
 import sys
 import subprocess
-nid = None
+# ------------------------
+# Declaring the var for id , pass and teacher's name
+nid = None  
 npass = None
 name = ""
+# Reading path.txt and extracting the path in which zoom is installed
 zoompath = Path("path.txt")
 zoompath = zoompath.read_text()
 zoompath = Path(zoompath,"Zoom.exe")
+# Reading info.txt and extracting the info about id and passes
 info = Path("info.txt")
 info = info.read_text()
 info = info.split('\n')
 
+# Takes in the teacher's name and returns their id and pass
 def manualJoin(name):
     for i in range(0,len(info)):
         t = info[i].split(':')
@@ -23,7 +29,10 @@ def manualJoin(name):
             nid = t[1]
             npass = t[2]
     return nid,npass,name
+# Reads the time_table.txt and and returns id , pass and teacher's name
+# according to the time table
 def autoJoin():
+    # Checking which day is today 
     if datetime.date.today().weekday() == 0:
         cd = "monday"
     if datetime.date.today().weekday() == 1:
@@ -36,12 +45,15 @@ def autoJoin():
         cd = "friday"
     if datetime.date.today().weekday() == 5:
         cd = "saturday"
-    ch = datetime.datetime.now().hour
+    #---------------------------------------
+    ch = datetime.datetime.now().hour # which hour is it now 
 
+    # Reads the time_table.txt and stores it in the var timetable as an Array
     timetable = Path('time_table.txt')
     timetable = timetable.read_text()
     timetable = timetable.split('\n')
 
+    # Extracting the id and pass
     for i in range(0,len(timetable)):
         r = timetable[i].split(':')
         if cd == r[0]:
@@ -52,23 +64,28 @@ def autoJoin():
         if name == t[0]:
             nid = t[1]
             npass = t[2]
+    # --------------------------
     return nid,npass,name
+
+
+# Asking the user for manual join or auto join
 userInput = input()
 if userInput == "auto":
     
     nid , npass , name = autoJoin()
 else :
     nid , npass , name = manualJoin(userInput)
+# --------------------------------------------
 print("joining class of " + name + " with id " + nid + " and pass " + npass)
 
 time.sleep(3)
 
 
-
-subprocess.Popen(zoompath)
+# TODO : use the zoompath var instead of hardcoded path
+subprocess.Popen("C:\\Users\\i ball\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")
 
 time.sleep(4)
-join = pyautogui.locateOnScreen("join_button.png")
+join = pyautogui.locateOnScreen("img/join_button.png")
 join = pyautogui.center(join)
 
 pyautogui.click(join.x,join.y)
