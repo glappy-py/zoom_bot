3bdf7ec46b22e6b5e71fda35c66549cae55822ef
GLAPPY <satwikv7@gmail.com>
2021-01-10 15:59:51 +0530

    removed bloat

--- zoom_bot.py
+++ zoom_bot.py
@@ -1,25 +1,24 @@
 # Importing useful modules
-import pathlib
-import datetime
-import pyautogui
-import pygetwindow as gw
-import keyboard
-import sys
+from pathlib import Path
+from datetime import date,datetime
+from pyautogui import center,locateOnScreen,click
+from keyboard import press,write
+from sys import exit
 from time import sleep
-import subprocess
+from subprocess import Popen
 # ------------------------
 # Declaring the var for id , pass and teacher's name
 nid = None  
 npass = None
 name = ""
 # Reading path.txt and extracting the path in which zoom is installed
-zoompath = pathlib.Path("txts\\path.txt")
+zoompath = Path("txts\\path.txt")
 zoompath = zoompath.read_text()
-zoompath = pathlib.Path(zoompath,"Zoom.exe")
+zoompath = Path(zoompath,"Zoom.exe")
 zoompath = str(zoompath)
 zoompath = zoompath.replace("\\","\\\\")
 # Reading info.txt and extracting the info about id and passes
-info = pathlib.Path("txts\\info.txt")
+info = Path("txts\\info.txt")
 info = info.read_text()
 info = info.split('\n')
 
@@ -35,23 +34,23 @@ def manualJoin(name):
 # according to the time table
 def autoJoin():
     # Checking which day is today 
-    if datetime.date.today().weekday() == 0:
+    if date.today().weekday() == 0:
         cd = "monday"
-    if datetime.date.today().weekday() == 1:
+    if date.today().weekday() == 1:
         cd = "tuesday"
-    if datetime.date.today().weekday() == 2:
+    if date.today().weekday() == 2:
         cd = "wednesday"
-    if datetime.date.today().weekday() == 3:
+    if date.today().weekday() == 3:
         cd = "thursday"
-    if datetime.date.today().weekday() == 4:
+    if date.today().weekday() == 4:
         cd = "friday"
-    if datetime.date.today().weekday() == 5:
+    if date.today().weekday() == 5:
         cd = "saturday"
     #---------------------------------------
-    ch = datetime.datetime.now().hour # which hour is it now 
+    ch = datetime.now().hour # which hour is it now 
 
     # Reads the time_table.txt and stores it in the var timetable as an Array
-    timetable = pathlib.Path('txts\\time_table.txt')
+    timetable = Path('txts\\time_table.txt')
     timetable = timetable.read_text()
     timetable = timetable.split('\n')
 
@@ -80,43 +79,42 @@ if userInput == "auto":
     except :
         print("you don't have any class right now")
         sleep(3)
-        sys.exit()
+        exit()
         
 else :
     nid , npass , name = manualJoin(userInput)
 # --------------------------------------------
-subprocess.Popen(zoompath) # launching zoom
+Popen(zoompath) # launching zoom
 
 # checking if zoom has launched yet
-while pyautogui.locateOnScreen("img\\join_button.png") == None and pyautogui.locateOnScreen("img\\join_a_meeting.png") == None:
+while locateOnScreen("img\\join_button.png") == None and locateOnScreen("img\\join_a_meeting.png") == None:
     print("waiting for zoom to launch")
     
 # checking if the user has signed in or not and locate the buttons accordingly
 try:
-    join = pyautogui.locateOnScreen("img\\join_button.png")
-    join = pyautogui.center(join)
-    pyautogui.click(join.x,join.y)
+    join = locateOnScreen("img\\join_button.png")
+    join = center(join)
+    click(join.x,join.y)
 except Exception as e:
-    join = pyautogui.locateOnScreen("img\\join_a_meeting.png")
-    join = pyautogui.center(join)
-    pyautogui.click(join.x,join.y)
+    join = locateOnScreen("img\\join_a_meeting.png")
+    join = center(join)
+    click(join.x,join.y)
 
 # showing class name , id and pass to user for cross correction
 print("joining class of " + name + " with id " + nid + " and pass " + npass)
 
 
 # waiting for zoom to load
-while pyautogui.locateOnScreen('img\\joining.png') == None:
+while locateOnScreen('img\\joining.png') == None:
     print("waiting...")
 
 
 # entering id and pass 
-keyboard.write(str(nid))
-pyautogui.press("enter")
-while pyautogui.locateOnScreen('img\\passcode.png') == None:
+write(str(nid))
+press("enter")
+while locateOnScreen('img\\passcode.png') == None:
     print("waiting...")
-keyboard.write(str(npass))
-pyautogui.press("enter")
+write(str(npass))
+press("enter")
 # --------------------
-
-sys.exit() # closing zoom-bot.py
+exit() # closing zoom-bot.py
